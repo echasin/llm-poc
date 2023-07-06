8f52a708adfffb5d1d09e0d6d2d1dbaeeb892f9d
Eric Chasin <echasin@innvosolutions.com>
2023-07-06 08:04:22 -0700

    Updated embeddings with .env

--- DQA embeddings.py
+++ DQA embeddings.py
@@ -1,8 +1,12 @@
-# Install Libraries
-# pip install unstructured --user
+# Issues
+# 1. If you rerun the is code it will upsert new embeddings into the index. 
+# 2. It creates 5880 Vectors
+# 3. How can set metadata in the pinecone index to stores the filename
+# 4. How can pinecone return metadata do we can track what documents have been processed in database
 
 # importing libraries
 import os
+from dotenv import load_dotenv
 import openai
 import pinecone
 import langchain
@@ -13,17 +17,23 @@ from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
-# OpenAI credentials
-openai_api_key = "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"
+# Load the environment variables from the .env file.
+load_dotenv()
 
+# Get the value of the API_KEY environment variable.
+
+# OpenAI credentials
+api_key = os.getenv('openai_api_key')
 # Pinecone credentials
-api_key = "ef9a9434-5233-4b29-a794-355b106be8d7"
-environment = "us-west4-gcp-free"
-index_name = "techplaybook-dev"
+
+api_key = os.getenv('api_key')
+environment = os.getenv('environment')
+index_name = os.getenv('index_name')
 
 # Source Files
 directory_path = '.\dataset'
 
+# Load the documents
 def load_docs(directory_path):
     loader = DirectoryLoader(directory_path)
     documents = loader.load()
@@ -44,18 +54,11 @@ print("Length of documents: ", len(docs))
 # Create the embeddings object
 embeddings = OpenAIEmbeddings(model ="text-embedding-ada-002") # type: ignore
 
-# Example
-query_result = embeddings.embed_query("large language model")
-len(query_result)
-print("query_result: ", query_result)
-
 #  Create the vector store object
 pinecone.init(
-    api_key="ef9a9434-5233-4b29-a794-355b106be8d7",
-    environment="us-west4-gcp-free"
-)
+    api_key  = api_key , # type: ignore
+    environment = environment) # type: ignore
 
-index_name = "techplaybook-dev"
+
 print("index_name: ", index_name)
 
-index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
